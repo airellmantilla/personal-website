3df9314f963cd00c2585f7f1365657dc6438aa8f
airellmantilla <airell.mantz@gmail.com>
2023-03-30 10:29:45 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+var mybutton = document.getElementById("topBtn");
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 300 ||
+    document.documentElement.scrollTop > 300
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
